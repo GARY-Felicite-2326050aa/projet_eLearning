57ab2f0bf1336e3eadaa933c4bb72772f266e242
Léo <leo.salenson@outlook.fr>
2026-02-03 11:36:42 +0100

    persist middleware pour rester conencter

--- front/src/features/auth/store/auth.sotre.ts
+++ front/src/features/auth/store/auth.sotre.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import type {User} from "../type/User";
+import { persist } from "zustand/middleware";
 
 type AuthState = {
     user: User | null;
@@ -7,8 +8,17 @@ type AuthState = {
     logout: () => void;
 };
 
-export const useAuthStore = create<AuthState>((set) => ({
-    user: null,
-    setUser: (user) => set({ user }),
-    logout: () => set({ user: null }),
-}));
+export const useAuthStore = create<AuthState>()(
+    persist(
+        (set) => ({
+            user: null,
+
+            setUser: (user) => set({ user }),
+
+            logout: () => set({ user: null }),
+        }),
+        {
+            name: "auth-storage",
+        }
+    )
+);
